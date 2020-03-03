c7d6bba239da9ff0ef575b546e04ff466f2ce979
dreyWesson <dreyWesson@gmail.com>
2020-03-03 10:37:12 +0100

    implemented click-out modal exit

--- modal-keytrap.js
+++ modal-keytrap.js
@@ -1,12 +1,11 @@
 // Will hold previously focused element
 var focusedElementBeforeModal;
-var body = document.querySelector('.dim');
-console.log(body)
 
-// Find the modal and its overlay
+// Find the modal, its overlay && toggle button
 var modal = document.querySelector('.modal');
-var modalOverlay = document.querySelector('.modal-overlay');
+var body = document.querySelector('.dim');
 
+var modalOverlay = document.querySelector('.modal-overlay');
 var modalToggle = document.querySelector('.modal-toggle');
 modalToggle.addEventListener('click', openModal);
 
@@ -22,6 +21,7 @@ function openModal() {
   // Sign-up button
   var signUpBtn = modal.querySelector('#signup');
   signUpBtn.addEventListener('click', closeModal);
+  body.addEventListener('click', closeModal);
 
   // Find all focusable children
   var focusableElementsString = 'a[href], area[href],input:not([disabled]), select:not([disabled]),textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable';
@@ -44,7 +44,7 @@ function openModal() {
 
   function trapTabKey(e) {
     // Check for TAB key press
-    if (e.keyCode ===9) {
+    if (e.keyCode === 9) {
       // SHIFT + TAB
       if(e.shiftKey) {
         if(document.activeElement === firstTabStop) {
