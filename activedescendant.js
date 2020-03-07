acf4ec1e3f090d5f657536f21812854a0b1fa87d
dreyWesson <dreyWesson@gmail.com>
2020-03-07 16:20:44 +0100

    created html custom element

--- activedescendant.js
+++ activedescendant.js
@@ -1,6 +1,6 @@
-const listbox = document.querySelector('[role="listbox"]')
-const characters = [...listbox.children]
-console.log(...listbox.children)
+const listbox = document.querySelector('[role="listbox"]');
+const characters = [...listbox.children];
+
 
 listbox.addEventListener('click', event => {
   const option = event.target.closest('li')
@@ -10,6 +10,14 @@ listbox.addEventListener('click', event => {
   listbox.setAttribute('aria-activedescendant', option.id)
 
   // Change visual appearance
-  characters.forEach(element => element.classList.remove('is-selected'))
+  characters.forEach(element => {
+    element.classList.remove('is-selected');
+    element.style.color ="black";
+    element.style.boxShadow = "0 0 0px 0px" 
+  })
+
+
   option.classList.add('is-selected')
+  option.style.color ="salmon";
+  option.style.boxShadow = "0 0 8px 3px rgb(103, 103, 139) " 
 })
